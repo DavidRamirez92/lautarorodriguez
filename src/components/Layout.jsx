b061d06fde75813cd14a674d962bfc1af2216bfe
David <davidramirez9210@gmail.com>
2021-06-19 14:46:38 -0300

    Layout bug fixed

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -62,7 +62,7 @@ const Layout = ( {children} ) => {
             <title>Lautaro Rodriguez Cineasta</title>
             <meta name="description" content="Portfolio de Lautaro Rodriguez cineasta y artista" />
 
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
 
             <link rel="preconnect" href="https://fonts.gstatic.com"/>
             <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&display=swap" rel="stylesheet"/>
