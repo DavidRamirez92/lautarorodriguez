eb82b2bf4ede94e36bd828e99f097f84cdea2cc8
David <davidramirez9210@gmail.com>
2021-06-22 12:00:06 -0300

    modify contact form destiny

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -15,7 +15,7 @@ const Contacto = () => {
 
             `}
             >
-                 <iframe title="contact Form" src="https://docs.google.com/forms/d/e/1FAIpQLSf68MjLFdjdrUWkjPFCUb15Jha2Sta0GO3NwLU6MmDSpqMeig/viewform?embedded=true" width="640" height="673" frameBorder="0" marginHeight="0" marginWidth="0">Cargando…</iframe>
+                 <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSciD0xd9bwQA8MfPnP0MOit252lnr2CixThHYUEA2hooaO9kg/viewform?embedded=true" width="640" height="673" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
             </main>
            
         </Layout>
